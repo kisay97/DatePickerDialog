f66b94505e8a370f31ac98c354a7ebeb212e77d0
최규광 <kisay97@naver.com>
2016-05-26 09:10:28 +0900

    20160526-0910

--- app/src/main/java/com/estsoft/datepickerdialog/MainActivity.java
+++ app/src/main/java/com/estsoft/datepickerdialog/MainActivity.java
@@ -1,7 +1,13 @@
 package com.estsoft.datepickerdialog;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dialogDatePicker(View view){
+        Calendar cal = Calendar.getInstance();
+        DatePickerDialog dialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.
+                OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Log.d("------------------->", year + "-" + (monthOfYear+1) + "-" + dayOfMonth);
+            }
+        }, cal.get( Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE) );
+
+        dialog.show();
     }
 }
